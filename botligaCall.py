7f83605636f083ee53f089f94bbc3eeda11efc86
johorst <johorst@mailinator.com>
2013-02-12 10:38:28 +0100

    Update botligaCall.py
    Konzept: String zusammenbauen und abfeuern

--- botligaCall.py
+++ botligaCall.py
@@ -1,2 +1,9 @@
 import json
 import urllib
+import os
+
+# command zusammenbauen: machtid und result (3. Stelle am Ausgabe Neuron) token s.u.
+mycmd = 'curl -X POST --data "match_id=14181&result=2:1&token=ofdcjlljsn6bv8r3lm1aoib2" http://botliga.de/api/guess'
+#    Execute the command (a string) in a subshell.
+os.system(command)
+
